c1b8d2eb45daf0a8e770a87f02eaef373a8e3d4f
André <andre.pereira@mportal.com.br>
2021-08-27 23:16:13 -0300

    applying open closed principle and dependency inversion principle

--- src/main/java/br/com/alura/rh/service/ReajusteService.java
+++ src/main/java/br/com/alura/rh/service/ReajusteService.java
@@ -1,21 +1,22 @@
 package br.com.alura.rh.service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.List;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 public class ReajusteService {
 	
+	private List<ValidacaoReajuste> validacoes;
+	
+	public ReajusteService(List<ValidacaoReajuste> validacoes) {
+		this.validacoes = validacoes;
+	}
+
 	public void reajustaSalario(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+		this.validacoes.forEach(v -> v.valida(funcionario, aumento));
 		
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0)
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		
-		funcionario.atualizaSalario(salarioAtual.add(aumento));
+		funcionario.atualizaSalario(funcionario.getSalario().add(aumento));
 	}
 
 }
